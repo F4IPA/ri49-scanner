dbaad5e56521ee4c6e5245a15b9710d5505c719d
Guillaume PLC <amonix75@gmail.com>
2023-05-18 13:26:18 +0200

    fix conflict

--- lib.py
+++ lib.py
@@ -5,7 +5,9 @@ import requests
 import sys
 import time
 
+
 current_room = ''
+last_room = 'rrf'
 counter = 0
 
 
@@ -16,33 +18,34 @@ def debug(text):
 
 def current_room_is_target(target):
     debug('Vérification de la room en cours')
-    file = open(current_room_path, 'r')
-    room = file.read().strip(); file.close()
+    room = get_current_room()
     return room == target
 
 
-def current_room_is_exclude(current_room_path, exclude_rooms):
+def current_room_is_exclude(exclude_rooms):
     debug('Vérication des rooms exclus')
-    file = open(current_room_path, 'r')
-    room = file.read().strip(); file.close()
+    room = get_current_room()
     return room in exclude_rooms
 
 
-def has_qsy(current_room_path):
+def get_current_room():
+    file = open('/etc/spotnik/network', 'r')
+    return file.read().strip(); file.close()
+
+
+def has_qsy():
     global current_room
-    file = open(current_room_path, 'r')
-    room = file.read().strip(); file.close()
+    room = get_current_room()
     result = room != current_room
     current_room = room
     debug(f'room actuelle: {room}')
     return result
     
 
-
-def has_traffic_in_current_room(log_path, sleep):
+def has_traffic_in_current_room(sleep):
     debug('Vérification du trafic dans la room actuelle')
     busy = False
-    log = open(log_path)
+    log = open('/tmp/svxlink.log')
     lines = log.readlines(); log.close()
     lines.reverse() if lines else lines
     for line in lines:
@@ -65,13 +68,14 @@ def has_traffic_in_target_room(api):
     return data['talker']
 
 
-def qsy_to(room, dtmf):
+def qsy_to(room):
+    global last_room
     print(f"Trafique détecté, QSY vers {room}", flush=True)
     last_room = get_current_room()
     os.system(f"nohup /etc/spotnik/restart.{room} &")
 
 
-def kill_and_start_timersalon():
+def kill_and_start_timersalon():    
     debug('kill and start timersalon')
     time.sleep(5)
     os.system("pkill -f timersalon")
--- main.py
+++ main.py
@@ -7,11 +7,8 @@ import lib
 import time
 
 TARGET   = 'ri49'                                        # salon à surveiller
-DTMF     = 49                                            # code DTMF du salon TARGET
 EXCLUDES = ['el','num','default']                        # salon où le scanner doit etre désactivé
 API      = 'https://49.f4ipa.fr/data/api.json'           # url de l'API du salon TARGET
-NETWORK  = '/etc/spotnik/network'                        # chemin vers le salon actuel
-SVXLOG   = '/tmp/svxlink.log'                            # chemin vers le fichier svxlink.log
 SLEEP    = 60                                            # secondes d'inactivées avant début du scan
 INTERVAL = 3                                             # Delai en secondes entre chaque scan
 
@@ -22,23 +19,23 @@ while True:
     time.sleep(INTERVAL)
 
     # en cas de qsy remettre le compteur de 5 minutes à 0
-    if lib.has_qsy(NETWORK): lib.counter = 0
+    if lib.has_qsy(): lib.counter = 0
 
     # vérification du salon actuel
-    if lib.current_room_is_target(NETWORK, TARGET): continue
+    if lib.current_room_is_target(TARGET): continue
 
     # vérification des salons exclus
-    if lib.current_room_is_exclude(NETWORK, EXCLUDES): continue
+    if lib.current_room_is_exclude(EXCLUDES): continue
 
 
     # On attend un delai avant de commancer à scanner
-    # Cela permet d'éviter certains problèmes d'informations manquantes
+    # Cela permet d'éviter certain problèmes d'informations manquantes
     # lors d'un qsy dans vers un nouveau salon. 
-    if lib.qsy_counter_complete(270, INTERVAL): continue
+    if lib.qsy_counter_complete(360, INTERVAL): continue
 
 
     # vérification si trafic dans le salon en cours
-    if lib.has_traffic_in_current_room(SVXLOG, SLEEP): continue
+    if lib.has_traffic_in_current_room(SLEEP): continue
 
 
     # Début du scan
@@ -46,8 +43,11 @@ while True:
 
 
     # on déménage sur la room en activité
-    lib.qsy_to(TARGET, DTMF)
+    lib.qsy_to(TARGET)
 
+
+    # on lance timersalon pour retour après inactivité
+    lib.kill_and_start_timersalon()
 
     # on lance timersalon pour retour après inactivité
     lib.kill_and_start_timersalon()
